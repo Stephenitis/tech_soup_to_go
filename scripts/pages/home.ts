4ac1e6c2933afe592e7588c1df58a83911c4f3e9
Stephen Nguyen <nguyenhstephen@gmail.com>
2013-06-29 01:07:35 -0700

    Removed fluff from homepage

--- scripts/pages/home.ts
+++ scripts/pages/home.ts
@@ -1,1 +1,38 @@
 # Place holder/example file
+
+$("//*[(@id = 'ctl00_ContentPlaceHolder_footer_MasterFooter_LinkCategoryList_ctrl3_divFooterItemMenu')]") {
+  remove()
+}
+$("//*[contains(concat( ' ', @class, ' ' ), concat( ' ', 'mod-wpz-4col', ' ' ))]") {
+  remove()
+}
+
+
+$("//*[contains(concat( ' ', @class, ' ' ), concat( ' ', 'grid-4col', ' ' ))]"){
+  remove()
+}
+
+$("//*[contains(concat( ' ', @class, ' ' ), concat( ' ', 's-ms-sub-sect-wrapper', ' ' ))]"){
+  remove()
+}
+
+$("//*[contains(concat( ' ', @class, ' ' ), concat( ' ', 'mod-second-nav', ' ' ))]"){
+  remove()
+}
+
+$("//*[contains(concat( ' ', @class, ' ' ), concat( ' ', 'bg-shadow-down-medium', ' ' ))]"){
+  remove()
+}
+$("
+//*[contains(concat( ' ', @class, ' ' ), concat( ' ', 'bg-footer-extra', ' ' ))]"){
+  remove()
+}
+$("//*[contains(concat( ' ', @class, ' ' ), concat( ' ', 's-fn-wrapper-top', ' ' ))]"){
+  remove()
+}
+
+
+
+
+
+
