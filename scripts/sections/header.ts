2f05f52bdd006f17869696eb4a55478813422311
Stephane Thomas <thomas.stephn@gmail.com>
2013-06-30 11:04:25 -0700

    Added iphone icon new file:   assets/images/techsoup.jpg modified:   scripts/sections/header.ts

--- scripts/sections/header.ts
+++ scripts/sections/header.ts
@@ -9,6 +9,7 @@ $('./body') {
 
 $("/html/head"){
   insert('link', href:"http://fonts.googleapis.com/css?family=Lato:100,300,400,700,900,100italic,300italic,400italic,700ita...' rel='stylesheet' type='text/css'")
+  insert('link', rel:"apple-touch-icon", href:asset("images/techsoup.jpg"))
 }
 #c0392b
 #16a085
