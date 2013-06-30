60dd48571f5e70c7b4e3d267c2c14b83fccf8273
Stephen Nguyen <nguyenhstephen@gmail.com>
2013-06-30 00:19:28 -0700

    added font script to body :(

--- scripts/sections/header.ts
+++ scripts/sections/header.ts
@@ -2,3 +2,7 @@ $('./body') {
   insert_top("header", class: "_header") {
   }
 }
+
+$("/html/head"){
+  insert("<link href='http://fonts.googleapis.com/css?family=Lato:100,300,400,700,900,100italic,300italic,400italic,700ita...' rel='stylesheet' type='text/css'>")
+}
